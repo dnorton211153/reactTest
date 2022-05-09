d780cc4e04efa1cb6cde40bd2b90a5b7d77affed
Dave Norton <dnorton211153@gmail.com>
2022-05-09 10:43:14 -0600

    Switch to Form for AWS API gateway

---
+++ src/Form.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      message: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const inputValue = event.target.value;
+    const stateField = event.target.name;
+    this.setState({
+      [stateField]: inputValue,
+    });
+    console.log(this.state);
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    const { name, message } = this.state;
+    await axios.post(
+      "https://6slzal5kh3.execute-api.us-west-1.amazonaws.com/default/serverlessAppFunction",
+      { key1: `${name}, ${message}` }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Leave your message here:</h1>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleChange}
+              value={this.state.name}
+            />
+
+            <label>Message:</label>
+            <input
+              type="text"
+              name="message"
+              onChange={this.handleChange}
+              value={this.state.message}
+            />
+
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
